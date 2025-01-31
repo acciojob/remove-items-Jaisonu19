5e953c08c8e195bcef30b15c6927b89bccc433e3
Jaisonu19 <134277404+Jaisonu19@users.noreply.github.com>
2025-01-31 14:37:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+// Get references to the dropdown and button
+var Colorselect = document.querySelector('#colorSelect');
+var removeButton = document.querySelector('#RemoveButton');
+
+// Add an event listener to the button
+removeButton.addEventListener('click', function () {
+    // Get the selected index
+    var selectIndex = Colorselect.selectedIndex;
+
+    // Check if an option is selected
+    if (selectIndex !== -1) {
+        // Remove the selected option
+        Colorselect.remove(selectIndex);
+    } else {
+        alert('Please select a color to remove.');
+    }
+});
